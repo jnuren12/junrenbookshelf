c92bd7ad9476b0093254c683a194886db89477a7
yddchsc <1145833162@qq.com>
2016-04-09 00:48:10 +0800

    str

--- book.php
+++ book.php
@@ -112,9 +112,9 @@ var dtGridColumns_2_1_2 = [
 }},
 {id:'operation', title:'操作', type:'string', columnClass:'text-center', resolution:function(value, record, column, grid, dataNo, columnNo){
 			 			var content = '';
-			 			content += '<form action="bookchange.php" method="post"><input type="hidden" name="bookname" value="'+record.bookname+'"/><input type="hidden" name="booknumber" value="'+record.booknumber+'"/><input type="hidden" name="author" value="'+record.author+'"/><input type="hidden" name="type" value="'+record.bookname+'"/><input type="hidden" name="status" value="'+record.bookname+'"/><button class="btn btn-xs btn-default" onclick="alert(\'编辑用户：'+record.book_name+'\');"><i class="fa fa-edit"></i>&nbsp;&nbsp;编辑</button></form>';
+			 			content += '<form action="bookchange.php" method="post"><input type="hidden" name="bookname" value="'+record.bookname+'"/><input type="hidden" name="booknumber" value="'+record.booknumber+'"/><input type="hidden" name="author" value="'+record.author+'"/><input type="hidden" name="type" value="'+record.type1.toString()+'"/><input type="hidden" name="status" value="'+record.status1.toString()+'"/><button class="btn btn-xs btn-default" type="submit"><i class="fa fa-edit"></i>&nbsp;&nbsp;编辑</button></form>';
 			 			content += '&nbsp;&nbsp;';
-			 			content += '<form action="bookdel.php" method="post"><input type="hidden" name="booknumber" value="'+record.booknumber+'"/><button class="btn btn-xs btn-danger" onclick="alert(\'删除用户：'+record.book_name+'\');"><i class="fa fa-trash-o"></i>&nbsp;&nbsp;删除</button></form>';
+			 			content += '<form action="bookdel.php" method="post"><input type="hidden" name="booknumber" value="'+record.booknumber+'"/><button class="btn btn-xs btn-danger" type="submit" onclick="alert(\'确定删除书本'+record.book_name+'？\');"><i class="fa fa-trash-o"></i>&nbsp;&nbsp;删除</button></form>';
 			 			return content;
 			 		}}
 
