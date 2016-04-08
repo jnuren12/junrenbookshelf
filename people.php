b2a1904fd0fb0bb3804341784d904a5f5b7e10ca
yddchsc <1145833162@qq.com>
2016-04-09 01:20:14 +0800

    try

--- people.php
+++ people.php
@@ -91,7 +91,7 @@ else{
 		book.status1 = ".$res['status'].";
 		book.phonenumber = '".$res['phonenumber']."';
 		book.roomnumber = '".$res['roomnumber']."';
-		book.time = '".$res['time']."';
+		book.time = ".$res['time'].";
 		datas.push(book);
 		";
 	}while($res=mysql_fetch_array($sql));
