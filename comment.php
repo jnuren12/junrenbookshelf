f16075712a325fd26b6f56304354550673c2cc1d
yddchsc <1145833162@qq.com>
2016-04-09 12:12:54 +0800

    check

--- comment.php
+++ comment.php
@@ -153,7 +153,9 @@ $(function(){
         <input class="info" type="submit"/ value="提交">
     </form>
 </div>
-<script src="jss/trumbowyg/trumbowyg.js"></script>
+<script src="http://libs.baidu.com/jquery/1.8.3/jquery.min.js"></script>
+<script>window.jQuery || document.write('<script src="js/vendor/jquery-1.10.2.min.js"><\/script>')</script>
+<script src="jss/trumbowyg/trumbowyg.min.js"></script>
 <script src="jss/trumbowyg/langs/fr.js"></script>
 <script src="jss/trumbowyg/plugins/upload/trumbowyg.upload.js"></script>
 <script src="jss/trumbowyg/plugins/base64/trumbowyg.base64.js"></script>
