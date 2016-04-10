b33d0f6cc21b44e164dbaf1d6deb7b18ca598d49
yddchsc <1145833162@qq.com>
2016-04-10 18:27:42 +0800

    finish

--- bookdel.php
+++ bookdel.php
@@ -3,9 +3,10 @@ header("Content-type: text/html; charset=utf-8");
 if($_POST){
     $booknumber=$_POST["booknumber"];
     include("connection.php");
-    $sql = mysql_query("DELETE from book where booknumber='$booknumber'");//执行插入语句
+    $sql = mysql_query("DELETE from book where booknumber='$booknumber'");
     if($sql){
          echo "<script>alert('删除成功');history.go(-1);</script>";
     }
+    mysql_query("DELETE from comment where booknumber='$booknumber'");
 }
  ?>
